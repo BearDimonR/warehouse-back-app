8387d01f812e0669e68a42bff6a8ddaea4ebf9c3
Maksym Slobodianyk <45465718+MaksymSlobodianyk@users.noreply.github.com>
2020-06-05 23:59:42 +0300

    Replace Properties in utils folder

--- src/main/java/com/warehouse/DAO/DataBaseConnector.java
+++ src/main/java/com/warehouse/DAO/DataBaseConnector.java
@@ -1,10 +1,9 @@
 package com.warehouse.DAO;
 
 import com.warehouse.ConnectionPool;
-import com.warehouse.DBProperties;
+import com.warehouse.utils.DBProperties;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DataBaseConnector {
--- src/main/java/com/warehouse/utils/DBProperties.java
+++ src/main/java/com/warehouse/utils/DBProperties.java
@@ -1,4 +1,4 @@
-package com.warehouse;
+package com.warehouse.utils;
 
 import java.io.FileInputStream;
 import java.io.IOException;
