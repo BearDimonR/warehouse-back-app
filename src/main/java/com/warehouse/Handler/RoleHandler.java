49882021f04c704e0ed8fb4cc756f05bd6b2b431
BearDimonR <48130903+BearDimonR@users.noreply.github.com>
2020-06-06 01:05:22 +0300

    finish roles
    - add post and put
    - deal with error codes

--- src/main/java/com/warehouse/Handler/RoleHandler.java
+++ src/main/java/com/warehouse/Handler/RoleHandler.java
@@ -3,18 +3,22 @@ package com.warehouse.Handler;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.warehouse.DAO.RoleDAO;
+import com.warehouse.JsonProceed;
 import com.warehouse.Model.Role;
 import com.warehouse.Splitter;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.security.InvalidParameterException;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Optional;
 
 public class RoleHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        switch (exchange.getRequestMethod()){
+        switch (exchange.getRequestMethod()) {
             case "GET":
                 getRole(exchange);
                 break;
@@ -31,39 +35,133 @@ public class RoleHandler implements HttpHandler {
                 System.err.println("Undefined request method!");
         }
     }
+
     private void getRole(HttpExchange exchange) throws IOException {
+        long id = Splitter.getId(exchange.getRequestURI());
+        if(id == -1)
+            getAllRoles(exchange);
+        else
+            getRole(exchange, id);
+    }
+
+    private void getAllRoles(HttpExchange exchange) throws IOException {
         try {
-            Optional<Role> role = RoleDAO.getInstance().get(Splitter.getId(exchange.getRequestURI()));
-            if(role.isEmpty())
+            List<Role> roles = RoleDAO.getInstance().getAll();
+            OutputStream os = exchange.getResponseBody();
+            String roleJson = JsonProceed.getGson().toJson(roles);
+            exchange.sendResponseHeaders(200, 0);
+            //Encrypt roles
+            os.write(roleJson.getBytes());
+            os.flush();
+            exchange.close();
+        } catch (IOException e) {
+            exchange.sendResponseHeaders(500, 0);
+            exchange.close();
+            System.err.println("Problem with getting role streams!");
+            throw e;
+        } catch (SQLException e) {
+            exchange.sendResponseHeaders(500, 0);
+            exchange.close();
+            System.err.println("Problem with server response when getting role");
+        }
+    }
+
+    private void getRole(HttpExchange exchange, long id) throws IOException {
+        try {
+            Optional<Role> role = RoleDAO.getInstance().get(id);
+            if (role.isEmpty())
                 throw new InvalidParameterException();
             OutputStream os = exchange.getResponseBody();
-            // make role json and convert to bytes
-            // os.write();
+            String roleJson = JsonProceed.getGson().toJson(role.get());
+            exchange.sendResponseHeaders(200, 0);
+            //Encrypt role
+            os.write(roleJson.getBytes());
             os.flush();
-            os.close();
+            exchange.close();
         } catch (IOException e) {
-            exchange.sendResponseHeaders(404, 0);
-            System.err.println("Problem with getting role!");
+            exchange.sendResponseHeaders(500, 0);
+            exchange.close();
+            System.err.println("Problem with getting role streams!");
             throw e;
         } catch (InvalidParameterException e) {
             exchange.sendResponseHeaders(404, 0);
+            exchange.close();
             System.err.println("Trying to access not created user");
+        } catch (SQLException e) {
+            exchange.sendResponseHeaders(500, 0);
+            exchange.close();
+            System.err.println("Problem with server response when getting role");
         }
     }
 
     private void putRole(HttpExchange exchange) throws IOException {
+        try {
+            InputStream is = exchange.getRequestBody();
+            byte[] input = is.readAllBytes();
+            // decode input array
+            Role role = JsonProceed.getGson().fromJson(new String(input), Role.class);
+            RoleDAO.getInstance().save(role);
+            exchange.sendResponseHeaders(200, 0);
+            exchange.close();
+        } catch (SQLException e) {
+            // check if it exception about unique name
+            if(e.getSQLState().equals("23505")) {
+                exchange.sendResponseHeaders(409, 0);
+                System.err.println("Such name already used!");
+            }
+            else {
+                exchange.sendResponseHeaders(500, 0);
+                System.err.println("Problem with server response when creating role");
+            }
+            exchange.close();
+        }
     }
 
     private void postRole(HttpExchange exchange) throws IOException {
-
+        try {
+            InputStream is = exchange.getRequestBody();
+            byte[] input = is.readAllBytes();
+            // decode input array
+            Role role = JsonProceed.getGson().fromJson(new String(input), Role.class);
+            if (!RoleDAO.getInstance().update(role, null))
+                throw new InvalidParameterException();
+            else
+                exchange.sendResponseHeaders(200, 0);
+            exchange.close();
+        } catch (InvalidParameterException e) {
+            exchange.sendResponseHeaders(404, 0);
+            exchange.close();
+            System.err.println("Trying to access not created user");
+        } catch (SQLException e) {
+            if(e.getSQLState().equals("23505")) {
+                exchange.sendResponseHeaders(409, 0);
+                System.err.println("Such name already used!");
+            }
+            else {
+                exchange.sendResponseHeaders(500, 0);
+                System.err.println("Problem with server response when creating role");
+            }
+            exchange.close();
+        }
     }
 
     private void deleteRole(HttpExchange exchange) throws IOException {
-
+        try {
+            InputStream is = exchange.getRequestBody();
+            byte[] input = is.readAllBytes();
+            // decode input array
+            Role role = JsonProceed.getGson().fromJson(new String(input), Role.class);
+            if(!RoleDAO.getInstance().delete(role))
+                exchange.sendResponseHeaders(404, 0);
+            else
+                exchange.sendResponseHeaders(200, 0);
+            exchange.close();
+        } catch (SQLException e) {
+            exchange.sendResponseHeaders(500, 0);
+            exchange.close();
+            System.err.println("Problem with server response when deleting role");
+        }
     }
-
-
-
 
 
 }
