89e3d912ba92d5c76bc7461de3f3d8f2238801ad
igorYevtushenko2000 <i11am22from2000ukraine@gmail.com>
2020-06-06 14:32:34 +0300

    [F-PERMISSION-CHAIN] add lombok to models

--- src/main/java/com/warehouse/Model/Permission.java
+++ src/main/java/com/warehouse/Model/Permission.java
@@ -2,60 +2,15 @@ package com.warehouse.Model;
 
 import java.util.Objects;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Permission {
     private int id;
     private String name;
     private boolean isSuper;
-
-    public Permission(int id, String name, boolean isSuper) {
-        this.id = id;
-        this.name = name;
-        this.isSuper = isSuper;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public boolean isIsSuper() {
-        return isSuper;
-    }
-
-    public void setIsSuper(boolean is_super) {
-        this.isSuper = is_super;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Permission permission = (Permission) obj;
-        return id == permission.id &&
-                Objects.equals(name, permission.name) &&
-                isSuper == permission.isSuper;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, isSuper);
-    }
-
-    public String toString() {
-        return "Permission { \nid = " + id +
-                ", \nname = " + name +
-                ", \nis_super = " + isSuper +
-                " }\n";
-    }
 }
--- src/main/java/com/warehouse/Model/Product.java
+++ src/main/java/com/warehouse/Model/Product.java
@@ -1,7 +1,12 @@
 package com.warehouse.Model;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
     private int id;
     private String name;
@@ -12,111 +17,4 @@ public class Product {
     private int groupProductId;
     private int manufactureId;
     private String description;
-
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-
-    public double getTotalCost() {
-        return totalCost;
-    }
-
-    public void setTotalCost(double totalCost) {
-        this.totalCost = totalCost;
-    }
-
-    public String getMeasureName() {
-        return measureName;
-    }
-
-    public void setMeasureName(String measureName) {
-        this.measureName = measureName;
-    }
-
-    public int getGroupProductId() {
-        return groupProductId;
-    }
-
-    public void setGroupProductId(int groupProductId) {
-        this.groupProductId = groupProductId;
-    }
-
-    public int getManufactureId() {
-        return manufactureId;
-    }
-
-    public void setManufactureId(int manufactureId) {
-        this.manufactureId = manufactureId;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Product product = (Product) obj;
-        return id == product.id &&
-                Objects.equals(name, product.name) &&
-                price ==product.price &&
-                amount == product.amount &&
-                totalCost == product.totalCost &&
-                Objects.equals(measureName, product.measureName) &&
-                groupProductId == product.groupProductId &&
-                manufactureId == product.manufactureId &&
-                Objects.equals(description, product.description);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, price, amount, totalCost, measureName, groupProductId, manufactureId, description);
-    }
-
-    public String toString() {
-        return "Permission { \nid = " + id + ", " +
-                "\nname = " + name + "" +
-                ", \nprice = " + price +
-                ", \namount = " + amount +
-                "\n total_cost = " + totalCost +
-                ", \nmeasureName = " + measureName  +
-                ", \ngroupProductId = " + groupProductId +
-                "\nmanufactureId = " + manufactureId  + "" +
-                "\ndescription = " + description +
-                "\n}\n";
-    }
 }
