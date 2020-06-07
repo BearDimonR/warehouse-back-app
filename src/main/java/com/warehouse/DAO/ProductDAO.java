ce838a3bedb9246eaa0852ce20833f55a33db735
igorYevtushenko2000 <i11am22from2000ukraine@gmail.com>
2020-06-07 21:29:07 +0300

    fixed bug with product -handler, -dao

--- src/main/java/com/warehouse/DAO/ProductDAO.java
+++ src/main/java/com/warehouse/DAO/ProductDAO.java
@@ -26,105 +26,120 @@ public class ProductDAO implements DAO<Product> {
     }
 
     @Override
-    public synchronized Optional<Product> get(long id) throws SQLException {
-        connection = DataBaseConnector.getInstance().getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM product WHERE id = ?");
-        preparedStatement.setLong(1, id);
-        ResultSet res = preparedStatement.executeQuery();
-        DataBaseConnector.getInstance().releaseConnection(connection);
-        connection = null;
-        if (res.next())
-            return Optional.of(new Product(
-                    res.getLong(1),
-                    res.getString(2),
-                    res.getDouble(3),
-                    res.getDouble(4),
-                    res.getDouble(5),
-                    res.getString(6),
-                    res.getInt(7),
-                    res.getInt(8),
-                    res.getString(9)));
+    public Optional<Product> get(long id) throws SQLException {
+        try {
+            connection = DataBaseConnector.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM product WHERE id = ?");
+            preparedStatement.setLong(1, id);
+            ResultSet res = preparedStatement.executeQuery();
+            if (res.next())
+                return Optional.of(new Product(
+                        res.getLong(1),
+                        res.getString(2),
+                        res.getDouble(3),
+                        res.getDouble(4),
+                        res.getDouble(5),
+                        res.getString(6),
+                        res.getInt(7),
+                        res.getInt(8),
+                        res.getString(9)));
 
-        return Optional.empty();
-    }
-
-    @Override
-    public synchronized List<Product> getAll() throws SQLException {
-        connection = DataBaseConnector.getInstance().getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM product");
-        ResultSet res = preparedStatement.executeQuery();
-        DataBaseConnector.getInstance().releaseConnection(connection);
-        connection = null;
-        List<Product> product = new ArrayList<>();
-        while (res.next()) {
-            product.add(new Product(
-                    res.getLong(1),
-                    res.getString(2),
-                    res.getDouble(3),
-                    res.getDouble(4),
-                    res.getDouble(5),
-                    res.getString(6),
-                    res.getInt(7),
-                    res.getInt(8),
-                    res.getString(9)));
+            return Optional.empty();
+        } finally {
+            DataBaseConnector.getInstance().releaseConnection(connection);
+            connection = null;
         }
-        return product;
     }
 
     @Override
-    public synchronized boolean save(Product product) throws SQLException {
-        connection = DataBaseConnector.getInstance().getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO product (name , price, amount, total_cost, measure_name, group_products_id, manufacturer_id, description) VALUES  (?,?,?,?,?,?,?,?)");
-
-        preparedStatement.setString(1, product.getName());
-        preparedStatement.setDouble(2, product.getPrice());
-        preparedStatement.setDouble(3, product.getAmount());
-        preparedStatement.setDouble(4, product.getTotalCost());
-        preparedStatement.setString(5, product.getMeasureName());
-        preparedStatement.setInt(6, product.getGroupProductId());
-        preparedStatement.setInt(7, product.getManufactureId());
-        preparedStatement.setString(8, product.getDescription());
-
-        int res = preparedStatement.executeUpdate();
-        DataBaseConnector.getInstance().releaseConnection(connection);
-        connection = null;
-        return res != 0;
+    public List<Product> getAll() throws SQLException {
+        try {
+            connection = DataBaseConnector.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM product");
+            ResultSet res = preparedStatement.executeQuery();
+            List<Product> product = new ArrayList<>();
+            while (res.next()) {
+                product.add(new Product(
+                        res.getLong(1),
+                        res.getString(2),
+                        res.getDouble(3),
+                        res.getDouble(4),
+                        res.getDouble(5),
+                        res.getString(6),
+                        res.getInt(7),
+                        res.getInt(8),
+                        res.getString(9)));
+            }
+            return product;
+        } finally {
+            DataBaseConnector.getInstance().releaseConnection(connection);
+            connection = null;
+        }
     }
 
     @Override
-    public synchronized boolean update(Product product, String[] params) throws SQLException {
-        connection = DataBaseConnector.getInstance().getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement("UPDATE product SET name=?,price=?, amount=?, total_cost=?, measure_name=?, group_products_id=?, manufacturer_id=?, description=? WHERE id=?");
-        preparedStatement.setString(1, product.getName());
-        preparedStatement.setDouble(2, product.getPrice());
-        preparedStatement.setDouble(3, product.getAmount());
-        preparedStatement.setDouble(4, product.getTotalCost());
-        preparedStatement.setString(5, product.getMeasureName());
-        preparedStatement.setInt(6, product.getGroupProductId());
-        preparedStatement.setInt(7, product.getManufactureId());
-        preparedStatement.setString(8, product.getDescription());
+    public boolean save(Product product) throws SQLException {
+        try {
+            connection = DataBaseConnector.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO product (name , price, amount, total_cost, measure_name, group_products_id, manufacturer_id, description) VALUES  (?,?,?,?,?,?,?,?)");
 
-        preparedStatement.setLong(9, product.getId());
+            preparedStatement.setString(1, product.getName());
+            preparedStatement.setDouble(2, product.getPrice());
+            preparedStatement.setDouble(3, product.getAmount());
+            preparedStatement.setDouble(4, product.getTotalCost());
+            preparedStatement.setString(5, product.getMeasureName());
+            preparedStatement.setInt(6, product.getGroupProductId());
+            preparedStatement.setInt(7, product.getManufactureId());
+            preparedStatement.setString(8, product.getDescription());
 
-        int res = preparedStatement.executeUpdate();
-        DataBaseConnector.getInstance().releaseConnection(connection);
-        connection = null;
-        return res != 0;
+            int res = preparedStatement.executeUpdate();
+            return res != 0;
+        } finally {
+            DataBaseConnector.getInstance().releaseConnection(connection);
+            connection = null;
+        }
     }
 
     @Override
-    public synchronized boolean delete(long id) throws SQLException {
-        connection = DataBaseConnector.getInstance().getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM product WHERE id=?");
-        preparedStatement.setLong(1, id);
+    public boolean update(Product product, String[] params) throws SQLException {
+        try {
+            connection = DataBaseConnector.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("UPDATE product SET name=?,price=?, amount=?, total_cost=?, measure_name=?, group_products_id=?, manufacturer_id=?, description=? WHERE id=?");
+            preparedStatement.setString(1, product.getName());
+            preparedStatement.setDouble(2, product.getPrice());
+            preparedStatement.setDouble(3, product.getAmount());
+            preparedStatement.setDouble(4, product.getTotalCost());
+            preparedStatement.setString(5, product.getMeasureName());
+            preparedStatement.setInt(6, product.getGroupProductId());
+            preparedStatement.setInt(7, product.getManufactureId());
+            preparedStatement.setString(8, product.getDescription());
 
-        int res = preparedStatement.executeUpdate();
-        DataBaseConnector.getInstance().releaseConnection(connection);
-        connection = null;
-        return res != 0;
+            preparedStatement.setLong(9, product.getId());
+
+            int res = preparedStatement.executeUpdate();
+            return res != 0;
+        } finally {
+            DataBaseConnector.getInstance().releaseConnection(connection);
+            connection = null;
+        }
     }
 
-    public synchronized List<Product> filter(String name, String group, String manufacturer) throws SQLException {
+    @Override
+    public boolean delete(long id) throws SQLException {
+        try {
+            connection = DataBaseConnector.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM product WHERE id=?");
+            preparedStatement.setLong(1, id);
+
+            int res = preparedStatement.executeUpdate();
+            return res != 0;
+        } finally {
+            DataBaseConnector.getInstance().releaseConnection(connection);
+            connection = null;
+        }
+    }
+
+    public List<Product> filter(String name, String group, String manufacturer) throws SQLException {
         Filter filter = new Filter() {
             @Override
             public ResultSet filterByName(String name) throws SQLException {
