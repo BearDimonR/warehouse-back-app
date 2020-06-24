ddd6c5f839c6f8067a9ed9ca900a25822af36700
BearDimonR <48130903+BearDimonR@users.noreply.github.com>
2020-06-24 03:27:55 +0300

    https
    add crypting

--- src/main/java/com/warehouse/Http/Server.java
+++ src/main/java/com/warehouse/Http/Server.java
@@ -1,6 +1,9 @@
 package com.warehouse.Http;
 
 import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpsConfigurator;
+import com.sun.net.httpserver.HttpsParameters;
+import com.sun.net.httpserver.HttpsServer;
 import com.warehouse.Controller.*;
 import com.warehouse.DAO.DataBaseConnector;
 import com.warehouse.View.JsonView;
@@ -8,8 +11,11 @@ import com.warehouse.View.View;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.net.ssl.*;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.security.KeyStore;
 import java.sql.SQLException;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.Executors;
@@ -58,10 +64,12 @@ public class Server {
 
             // Server start-up
             try {
-                HttpServer server = HttpServer.create();
+                HttpsServer server = HttpsServer.create();
+                
+                //Init keys
+                initHttps(server);
 
                 //Server contexts
-                //TODO Maybe Enum for this shit?
                 server.createContext("/products", new ProductController());
                 server.createContext("/groups", new GroupController());
                 server.createContext("/manufacturers", new ManufacturerController());
@@ -83,6 +91,51 @@ public class Server {
         } catch (Exception e) {
             root.fatal("Critical server exception: " + e.getMessage());
             System.exit(-1);
+        }
+    }
+
+    private void initHttps(HttpsServer server) {
+        try {
+
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+
+            // initialise the keystore
+            char[] password = "server".toCharArray();
+            KeyStore ks = KeyStore.getInstance("JKS");
+            FileInputStream fis = new FileInputStream("server.jks");
+            ks.load(fis, password);
+
+            // setup the key manager factory
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+            kmf.init(ks, password);
+
+            // setup the trust manager factory
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+            tmf.init(ks);
+
+            // setup the HTTPS context and parameters
+            sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+            server.setHttpsConfigurator(new HttpsConfigurator(sslContext) {
+                public void configure(HttpsParameters params) {
+                    try {
+                        // initialise the SSL context
+                        SSLContext context = getSSLContext();
+                        SSLEngine engine = context.createSSLEngine();
+                        params.setNeedClientAuth(false);
+                        params.setCipherSuites(engine.getEnabledCipherSuites());
+                        params.setProtocols(engine.getEnabledProtocols());
+
+                        // Set the SSL parameters
+                        SSLParameters sslParameters = context.getSupportedSSLParameters();
+                        params.setSSLParameters(sslParameters);
+
+                    } catch (Exception ex) {
+                        System.out.println("Failed to create HTTPS port");
+                    }
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
